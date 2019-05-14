1468202709b037c78063e55a014064c2e50bb5ed
Wes <48079826+Px0006@users.noreply.github.com>
2019-05-14 17:12:48 -0400

    type hints
    new element of Python found today

---
+++ StaticTypedPython.py
@@ -0,0 +1,9 @@
+Python 3.7.3 (default, Apr  3 2019, 05:39:12) 
+[GCC 8.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def sentence_string(sentence: str) -> bool:
+	return "animal" in sentence
+
+>>> sentence_string("there is a farm with animals")
+True
+>>> 
